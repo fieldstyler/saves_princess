b2d4a5cc6bf8f85d2cd6b82298dabd6b82943570
fieldstyler <tdfields93@gmail.com>
2021-04-09 15:55:55 -0600

    Write initial happy path test in save_princess_2

---
+++ spec/save_princess_2_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+require './lib/save_princess_2'
+
+RSpec.describe "Save Princess 2" do
+  it "Provides steps to save princess when given valid inputs" do
+    expect(display_path_to_princess(5, 2, 3, "-----\n-----\n---mp\n-----\n-----")).to eq("RIGHT")
+  end
+end
