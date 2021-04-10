d1d05509ef3e44239d9ba92ec0a4d9f6876b0411
fieldstyler <tdfields93@gmail.com>
2021-04-10 13:54:37 -0600

    Update spec with correct function name

--- spec/save_princess_2_spec.rb
+++ spec/save_princess_2_spec.rb
@@ -21,18 +21,18 @@ RSpec.describe "Save Princess 2" do
   end
 
   it "Returns a message if the n-value input is invalid" do
-    expect(display_path_to_princess(-3, -5, -5, "---\n-m-\n--p")).to eq("n must be a number between 2 and 100")
-    expect(display_path_to_princess(105, 20, 34, "---\n-m-\n--p")).to eq("n must be a number between 2 and 100")
+    expect(next_move(-3, -5, -5, "---\n-m-\n--p")).to eq("n must be a number between 2 and 100")
+    expect(next_move(105, 20, 34, "---\n-m-\n--p")).to eq("n must be a number between 2 and 100")
   end
 
   it "Returns a message if the r-value or c-value inputs are invalid" do
-    expect(display_path_to_princess(5, 0, 6, "---\n-m-\n--p")).to eq("r and c must be able to fit on the grid aka smaller than n-1 and greater than or equal to zero")
-    expect(display_path_to_princess(4, 1, -1, "---\n-m-\n--p")).to eq("r and c must be able to fit on the grid aka smaller than n-1 and greater than or equal to zero")
+    expect(next_move(5, 0, 6, "---\n-m-\n--p")).to eq("r and c must be able to fit on the grid aka smaller than n-1 and greater than or equal to zero")
+    expect(next_move(4, 1, -1, "---\n-m-\n--p")).to eq("r and c must be able to fit on the grid aka smaller than n-1 and greater than or equal to zero")
   end
 
   it "Returns a message if the grid input is invalid" do
-    expect(display_path_to_princess(11, 3, 2, [3, 'a', true])).to eq("grid must be a string")
-    expect(display_path_to_princess(3, 2, 2, "---\n---\n--m")).to eq("mario 'm' and princess 'p' must both be on the grid")
-    expect(display_path_to_princess(3, 2, 2, "---\n---\n--p")).to eq("mario 'm' and princess 'p' must both be on the grid")
+    expect(next_move(11, 3, 2, [3, 'a', true])).to eq("grid must be a string")
+    expect(next_move(3, 2, 2, "---\n---\n--m")).to eq("mario 'm' and princess 'p' must both be on the grid")
+    expect(next_move(3, 2, 2, "---\n---\n--p")).to eq("mario 'm' and princess 'p' must both be on the grid")
   end
 end
